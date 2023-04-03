95ca81e7db44e318bce8081bce1a6f2c61d4a53b
Amos Thando Mpofu <56162910+theliteprogrammer@users.noreply.github.com>
2023-04-03 21:49:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    private static int getFibonacciLastDigitFast(int n) {
+        int array[] = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i < array.length; ++i) {
+            array[i] = array[i - 2] + array[i - 1];
+        }
+        return array[n] % 10;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int c = getFibonacciLastDigitFast(n);
+        System.out.println(c);
     }
 }
